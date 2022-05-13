25387068ee8bac446450462afb0662697833d7da
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-05-14 01:09:13 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+
+const DIRNAME = path.join(__dirname, 'secret-folder');
+
+const readDir = async (dirname) => {
+	const files = await readdir(dirname, { withFileTypes: true });
+
+	for (const file of files) {
+		if (file.isFile()) {
+			fs.stat(path.join(dirname, file.name), (err, stats) => {
+				if (err) throw err;
+
+				let nameFile = path.parse(file.name).name;
+				let extFile = path.parse(file.name).ext.slice(1);
+				let sizeKbFile = stats.size / 100;
+				
+				console.log(`${nameFile} - ${extFile} - ${sizeKbFile}kb`);
+			});
+		}
+	}
+};
+
+readDir(DIRNAME);
