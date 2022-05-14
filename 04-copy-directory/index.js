6f70d2bed2a5b557efb3a4eadf583fd99360de1b
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-05-14 12:59:05 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const path = require('path');
+const { mkdir, readdir, copyFile } = require('fs/promises');
 
+const nameDirSrc = 'files';
+const nameDirDest = `${nameDirSrc}-copy`;
+
+const pathSrc = path.resolve(__dirname, nameDirSrc);
+const pathDest = path.resolve(__dirname, nameDirDest);
+
+async function copyDirectory(src, dest) {
+	await mkdir(dest, { recursive: true });
+	
+	const files = await readdir(src, { withFileTypes: true });
+	
+	for (const { name } of files) {
+		copyFile(path.resolve(src, name), path.resolve(dest, name));
+	}
+}
+
+copyDirectory(pathSrc, pathDest);
