6be5fd57e9b47378100390e00ae77c0d12dcefa3
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-05-23 23:20:20 +0300

    fix: remove regexp in task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,15 +5,14 @@ const { readdir } = require('fs/promises');
 const srcDir = path.join(__dirname, 'styles');
 const destDir = path.join(__dirname, 'project-dist');
 
-const regexp = /(\.css)$/gm;
-
 async function mergeStyles(src, dest) {
 	try {
 		const files = await readdir(src, { withFileTypes: true });
 		const writebleStream = fs.createWriteStream(path.join(dest, 'bundle.css'));
 
 		for (const file of files) {
-			if (regexp.test(file.name) && file.isFile()) {
+			const extention = file.name.split('.')[1];
+			if (extention === 'css' && file.isFile()) {
 				const readableStream = fs.createReadStream(path.join(src, file.name), 'utf-8');
 
 				readableStream.on('data', (chunk) => {
