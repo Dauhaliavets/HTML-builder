5aaade78eee01cfcc9f1772aa9e9484adce79b04
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-05-23 22:42:46 +0300

    fix: fix copy directory after remove files in task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { mkdir, readdir, copyFile } = require('fs/promises');
+const { mkdir, readdir, copyFile, rm } = require('fs/promises');
 
 const nameDirSrc = 'files';
 const nameDirDest = `${nameDirSrc}-copy`;
@@ -8,6 +8,7 @@ const pathSrc = path.resolve(__dirname, nameDirSrc);
 const pathDest = path.resolve(__dirname, nameDirDest);
 
 async function copyDirectory(src, dest) {
+	await rm(dest, { force: true, recursive: true });
 	await mkdir(dest, { recursive: true });
 	
 	const files = await readdir(src, { withFileTypes: true });
