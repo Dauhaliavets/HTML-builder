957afd657538d0c97d24af489169034483b6ca4a
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-05-23 22:35:09 +0300

    fix: fix correct show size in task 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,10 +11,10 @@ const readDir = async (dirname) => {
 		if (file.isFile()) {
 			fs.stat(path.join(dirname, file.name), (err, stats) => {
 				if (err) throw err;
-
+				
 				let nameFile = path.parse(file.name).name;
 				let extFile = path.parse(file.name).ext.slice(1);
-				let sizeKbFile = stats.size / 100;
+				let sizeKbFile = (stats.size / 1024).toFixed(3);
 				
 				console.log(`${nameFile} - ${extFile} - ${sizeKbFile}kb`);
 			});
